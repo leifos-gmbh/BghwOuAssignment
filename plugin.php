bf42326a73469d814995170d5069128210a14ef3
Marvin Barz <barz@leifos.de>
2021-11-05 16:41:57 +0100

    Initial ILIAS 7 compatibility

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 // alphanumerical ID of the plugin; never change this
 $id = "bghwldap";
 // code version; must be changed for all code changes
-$version = '5.4.18.1';
+$version = '7.3.0';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.8";
-$ilias_max_version = "5.4.999";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
